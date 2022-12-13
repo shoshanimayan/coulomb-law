cf9547fbcbef463cfa6e64659074841938459013
shoshanimayan <mshoshan@uci.edu>
2022-12-13 00:00:15 -0800

    cleanup for vr

--- Assets/Scripts/Particles/ParticleManager.cs
+++ Assets/Scripts/Particles/ParticleManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,13 @@ namespace Particles
     public class ParticleManager : MonoBehaviour
     {
 
+        #region Inpsector Elements
+        [Tooltip("particle prefab")]
+        [SerializeField] private GameObject _prefab;
+
+
+        #endregion
+
         #region Static Variables
 
         //static variable implementation to get all particles to every other particle
@@ -19,6 +27,12 @@ namespace Particles
 
         //material for postivie and negative particles
         private static Material s_positiveMaterial, s_negativeMaterial;
+
+        #endregion
+
+        #region Private Variables
+
+        private ParticleType _particleType = ParticleType.Postive;
         
         #endregion
 
@@ -30,6 +44,33 @@ namespace Particles
             s_particles = new List<Particle>( FindObjectsOfType(typeof(Particle)) as Particle[]);
             s_positiveMaterial = Resources.Load<Material>("Materials/Positive");
             s_negativeMaterial= Resources.Load<Material>("Materials/Negative") ;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CreateParticle(Vector3 position, Quaternion rotation)
+        {
+            position = new Vector3(position.x, position.y, 0);
+            var particle = Instantiate(_prefab, position, rotation);
+            var charge = 0;
+            switch (_particleType)
+            {
+                case ParticleType.Postive:
+                    charge = 1;
+                    break;
+                case ParticleType.Negative:
+                    charge = -1;
+                    break;
+            }
+            particle.GetComponent<Particle>().SetCharge(charge);
+            AddParticle(particle);
+        }
+
+        private void AddParticle(GameObject ParticleObject)
+        {
+            s_particles.Add(ParticleObject.GetComponent<Particle>());
         }
 
         #endregion
@@ -64,13 +105,17 @@ namespace Particles
             return s_negativeMaterial;
         }
 
-        public static void AddParticle(GameObject ParticleObject)
+      
+
+        public void SetParticleType(int index)
         {
-            s_particles.Add(ParticleObject.GetComponent<Particle>());
-           
-
-
+            if (index >= Enum.GetNames(typeof(ParticleType)).Length)
+            {
+                throw new Exception("index out of range");
+            }
+            _particleType = (ParticleType)index;
         }
+
 
         #endregion
 
